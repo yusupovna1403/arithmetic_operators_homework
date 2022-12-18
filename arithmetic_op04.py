86c1e2073380857c31070715634009c1b5faaa59
yusupovna1403 <yusupovna2003girl@gmail.com>
2022-12-18 14:45:23 +0500

    print4

--- arithmetic_op04.py
+++ arithmetic_op04.py
@@ -9,3 +9,9 @@
 #Create a variable called 'answer' and assign it the sum of the three digits x1, x2, x3.
 
 #Print the value of the 'answer.
+number = 123
+x1 = number % 10
+x2 = number // 10 % 10
+x3 = number // 100
+answer = x1 + x2 + x3
+print(answer)
