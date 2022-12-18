47dd6992b63fa2bb24dbc7891a544c6c6d1f178c
yusupovna1403 <yusupovna2003girl@gmail.com>
2022-12-18 14:42:02 +0500

    print3

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -3,3 +3,8 @@
 #Find the reverse of the number and assign it to a variable called 'answer'.
 
 #Print the value of the 'answer'
+number = 34
+a = number %10
+b = number // 10
+answer = a * 10 + b
+print(answer)
