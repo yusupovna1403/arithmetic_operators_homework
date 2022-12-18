49eb43914842d1c72457aa1520d6c3f86651a0d2
yusupovna1403 <yusupovna2003girl@gmail.com>
2022-12-18 15:05:36 +0500

    print5

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -3,8 +3,6 @@
 #Create a variable called 'answer' and assign the remainder of the division of number by 3 to it.
 
 #Print 'answer'.
-number1 = 89
-number2 = -89
-answer1 = number1 % 3
-answer2 = number2 % 3
-print("answer1 =",answer1,'\n',"answer2 =",answer2)
+number = -89
+answer = number % 3
+print(answer)
